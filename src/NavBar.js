a631cb99e046c00936c550f2682db53b0292c70e
Trevor Sabo <sabotrev@gmail.com>
2019-10-16 22:09:27 -0400

    Added color.js Added animation to emoji

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
+import colors from "./colors";
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg)
+  }
+`;
 
 const NavBar = () => (
   <header
     css={css`
-      background-color: #333;
+      background-color: ${colors.secondary};
       padding: 15px;
     `}
   >
     <Link to="/">Adopt Me!</Link>
-    <span role="img" aria-label="logo">
+    <span
+      css={css`
+        font-size: 60px;
+        display: inline-block;
+        animation: 1s ${spin} linear infinite;
+        &:hover {
+          text-decoration: underline;
+        }
+      `}
+      role="img"
+      aria-label="logo"
+    >
       🐶
     </span>
   </header>
---
+++ src/colors.js
@@ -0,0 +1,6 @@
+export default {
+  primary: "#bf3334",
+  secondary: "#d9c148",
+  darK: "#122622",
+  light: "#81a69b"
+};
