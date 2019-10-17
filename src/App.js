9c685d8ebc5f76e27bac7c92baeed78a6f505575
Trevor Sabo <sabotrev@gmail.com>
2019-10-16 22:32:44 -0400

    Added code splitting with React's lazy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@reach/router";
-import Details from "./Details";
 import NavBar from "./NavBar";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
 
 const App = () => {
   const theme = useState("darkblue");
@@ -12,10 +13,12 @@ const App = () => {
     <ThemeContext.Provider value={theme}>
       <div>
         <NavBar />
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <Suspense fallback={<h1>Loading route...</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
