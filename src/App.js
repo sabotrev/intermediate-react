5cf52e58275d9a5890d21c052fe34dc5f77ab995
Trevor Sabo <sabotrev@gmail.com>
2019-10-17 20:45:31 -0400

    Added express server

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useState, lazy, Suspense } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Router } from "@reach/router";
-import NavBar from "./NavBar";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
-
-const Details = lazy(() => import("./Details"));
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <NavBar />
-        <Suspense fallback={<h1>Loading route...</h1>}>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
-        </Suspense>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        ;
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </ThemeContext.Provider>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
